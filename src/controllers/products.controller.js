a397b3c4f8721e0566477c8ee4e356d2fbbfa249
RoqueCampoverde <94024025+RoqueCampoverde@users.noreply.github.com>
2022-02-25 02:39:05 -0500

    Práctica 2

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,44 +1,53 @@
-import { param } from 'express/lib/request';
-import { status } from 'express/lib/response';
-import Products from '../models/Product'
+import Product from "../models/Product";
 
-export const createProducts = async (req,res) => {
-    
-    const {name, category, price, imgURL} = req.body
+export const createProduct = async (req, res) => {
+  const { name, category, price, imgURL } = req.body;
 
-    const newProduct = new Products({name, category, price, imgURL});
+  try {
+    const newProduct = new Product({
+      name,
+      category,
+      price,
+      imgURL,
+    });
 
-    const productSaved = await newProduct.save()
-    
-    res.status(201).json(productSaved)
-}
+    const productSaved = await newProduct.save();
 
-export const getProducts = async (req,res) => {
-    const products = await Products.find();
-    res.json(products)
+    res.status(201).json(productSaved);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
 
+export const getProductById = async (req, res) => {
+  const { productId } = req.params;
 
-}
+  const product = await Product.findById(productId);
+  res.status(200).json(product);
+};
 
-export const getProductsById = async (req,res) => {
-    const product = await Products.findById(req.params.productId);
-    res.status(200).json(product)
+export const getProducts = async (req, res) => {
+  const products = await Product.find();
+  return res.json(products);
+};
 
+export const updateProductById = async (req, res) => {
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.productId,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  res.status(204).json(updatedProduct);
+};
 
-}
+export const deleteProductById = async (req, res) => {
+  const { productId } = req.params;
 
-export const updateProductsById = async (req,res) => {
-    const updateProduct = await Products.findByIdAndUpdate(req.params.productId, req.body, {
-        new: true
-    })
-    res.status(200).json(updateProduct)
+  await Product.findByIdAndDelete(productId);
 
-}
-
-export const deleteProductsById = async (req,res) => {
-    const {productId} = req.params;
-    await Products.findByIdAndDelete(productId) 
-        
-    res.status(204).json()
-
-}
+  // code 200 is ok too
+  res.status(204).json();
+};
--- src/libs/initialSetup.js
+++ src/libs/initialSetup.js
@@ -5,10 +5,13 @@ import bcrypt from "bcryptjs";
 
 export const createRoles = async () => {
   try {
+    // Count Documents
     const count = await Role.estimatedDocumentCount();
 
+    // check for existing roles
     if (count > 0) return;
 
+    // Create default Roles
     const values = await Promise.all([
       new Role({ name: "user" }).save(),
       new Role({ name: "moderator" }).save(),
@@ -18,5 +21,23 @@ export const createRoles = async () => {
     console.log(values);
   } catch (error) {
     console.error(error);
+  }
+};
+
+export const createAdmin = async () => {
+  // check for an existing admin user
+  const user = await User.findOne({ email: "admin@localhost" });
+  // get roles _id
+  const roles = await Role.find({ name: { $in: ["admin", "moderator"] } });
+
+  if (!user) {
+    // create a new admin user
+    await User.create({
+      username: "admin",
+      email: "admin@localhost",
+      password: await bcrypt.hash("admin", 10),
+      roles: roles.map((role) => role._id),
+    });
+    console.log('Admin User Created!')
   }
 };
