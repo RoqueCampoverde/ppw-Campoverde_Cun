99daba57b7cae4e12334ebeab92106c12eec0ca8
RoqueCampoverde <94024025+RoqueCampoverde@users.noreply.github.com>
2022-02-24 19:46:21 -0500

    Práctica 2

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User'
 import jwt from 'jsonwebtoken'
 import config from '../config'
+import Role from '../models/Role';
 
 export const signUp = async (req, res) => {
 
@@ -12,7 +13,16 @@ export const signUp = async (req, res) => {
         password: await User.encryptPassword(password)
     })
     
+    if (roles) {
+        const foundRoles = await Role.find({name: {$in: roles}})
+        newUser.roles = foundRoles.map(role => role._id)
+    }else {
+        const role = await Role.findOne({name: "user"})
+        newUser.roles = [role._id];
+    }
+
     const savedUser = await newUser.save();
+    console.log(savedUser)
 
     const token = jwt.sign({id: savedUser._id}, config.SECRET, {
         expiresIn: 86400 
@@ -21,5 +31,11 @@ export const signUp = async (req, res) => {
 }
 
 export const signIn = async (req, res) => {
-    res.json('signin')
+    const userFound = await User.findOne({email: req.body.email})
+
+    if (!userFound) return res.status(400).json({message: "User not found"})
+
+    console.log(userFound)
+
+    res.json({token: ""})
 }
